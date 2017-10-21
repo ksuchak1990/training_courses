fae4c44340f5942632b68cdebb8937353d624410
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2017-10-21 10:42:44 +0100

    basic functionality

--- CDRCWorker.py
+++ CDRCWorker.py
@@ -5,20 +5,37 @@ class CDRCWorker(WebWorker):
     """worker to grab the CDRC info, process it, and output it"""
     def __init__(self):
         super().__init__()
-        self.stages = {'catalogueDownload': self.downloadCatalogue, 'catalogueParse': self.parseCatalogue,
-                        'eventsDownload': self.downloadEvents, 'eventsParse': self.parseEvents}
+        self.stageList = ['catalogueDownload', 'catalogueParse', 'eventsDownload', 'eventsParse']
+        self.stageDict = {'catalogueDownload': self.downloadCatalogue, 'catalogueParse': self.parseCatalogue, 
+                            'eventsDownload': self.downloadEvents, 'eventsParse': self.parseEvents}
+        self.product = 'CDRC'
+        self.baseUrl = 'https://www.cdrc.ac.uk/events/'
+        self.eventSplitter = '<!-- Event  -->'
+        self.metadataSplitter = ''
+        self.headerDict = {'a': 'Title', 'b': 'Date & Time', 'c': 'Venue', 'd': 'Description'}
 
     def downloadCatalogue(self):
+        return self.requestURL(self.baseUrl)
+
+    def parseCatalogue(self, code):
+        restrictedCode = self.restrict(inputString=code, start='<!-- Events Loop -->', end='<!-- List Footer -->')
+
+        eventList = restrictedCode.split(self.eventSplitter)[1:]
+        urlList = [self.restrict(inputString=event, start='tribe-event-url" href="', end='" title=') for event in eventList]
+        return urlList
+
+    def downloadEvents(self, urlList):
+        return [self.requestURL(url) for url in urlList]
+
+    def parseEvents(self, code):
+        ## Parse the event page to get the event metadata
+        ## Isolate required text
+        # restrictedCode = self.restrict(inputString=code, start='', end='')
+        
+        ## Split into sections
+        # metadataList = restrictedCode.split(self.metadataSplitter)[1:]
+
+        ## Clean up sections
+        ## Make dictionary of metadata
         pass
 
-    def parseCatalogue(self):
-        pass
-
-    def downloadEvents(self):
-        pass
-
-    def parseEvents(self):
-        pass
-
-    def work(self):
-        super().work()
