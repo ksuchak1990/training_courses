b447f3329d5ea6bc5a4868a096dc0e98b9126d2e
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2017-10-21 10:42:22 +0100

    creating output directories

--- Worker.py
+++ Worker.py
@@ -1,14 +1,30 @@
+import os
+
 class Worker():
     """class representing the generic worker, for which more specific workers inherit"""
     def __init__(self):
-        self.stages = dict()
+        self.product = 'generic'
+        self.stageList = list()
+        self.stageDict = dict()
+        self.baseDir = 'output'
 
     def work(self):
-        print(self.stages)
-        for k, v in self.stages.items():
-            ## run the relevant function
-            pass
+        print(self.stageList)
+        print(self.stageDict)
+        # for stage in self.stageList:
+        #     self.stageDict[stage]()
+        #     pass
 
+    # ensure that relevant output directories exist
     def initialChecks(self):
-        ## for each stage, make sure that there is the relevant folder in which to output
+        outputPath = '{0}/{1}'.format(self.baseDir, self.product) if self.product != 'generic' else self.baseDir
+        if not os.path.exists(outputPath):
+            os.makedirs(outputPath)
+
+    # get data from previous stage
+    def pickUp():
+        pass
+
+    # output data at end of stage
+    def putDown():
         pass
